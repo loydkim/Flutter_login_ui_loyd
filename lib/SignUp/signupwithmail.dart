fb742be905650a5c4472ebf6626a8326113ba6c3
Loydkim <kimloydmail@gmail.com>
2020-05-16 17:34:31 -0600

    Update signupwithmail.dart

--- lib/SignUp/signupwithmail.dart
+++ lib/SignUp/signupwithmail.dart
@@ -216,22 +216,4 @@ class _SignUpWithMail extends State<SignUpWithMail> {
       return null;
     });
   }
-
-  void _update() async {
-    // row to update
-    Map<String, dynamic> row = {
-      DatabaseHelper.columnId   : 1,
-      DatabaseHelper.columnName : 'Mary',
-      DatabaseHelper.columnAge  : 32
-    };
-    final rowsAffected = await dbHelper.update(row);
-    print('updated $rowsAffected row(s)');
-  }
-
-  void _delete() async {
-    // Assuming that the number of rows is the id for the last row.
-    final id = await dbHelper.queryRowCount();
-    final rowsDeleted = await dbHelper.delete(id);
-    print('deleted $rowsDeleted row(s): row $id');
-  }
 }
